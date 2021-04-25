fadbd7606e4253884d0c2842d00cb336bffd124c
Qusay114 <eng.qusay015@gmail.com>
2021-04-25 23:12:56 +0300

    Feature Tasks

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
     render() {
         return (
-            <h1>Hello this is my first react app</h1>
+            <h1>Hello this is my first React App</h1>
         );
     }
 }
--- src/components/main.js
+++ src/components/main.js
@@ -4,7 +4,7 @@ import HornedBeast from './HornedBeast';
 class Main extends React.Component{
     render(){
         return(
-            <div>
+            <div id='container'>
             <HornedBeast name={'Dog'} description={'This is a dog'} src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzX3f-ls8hHhI-Zevpj4fbTEBUzK2b4YF6pA&usqp=CAU'} title={'random image'}/>
             <HornedBeast name={'Cat'} description={'This is a cat'} src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLWo0u-TB5eyu88DkM8Gvc6vWOOQlXxjSmAw&usqp=CAU'}/>
             </div>
